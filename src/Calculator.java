2dc3016716d75fdd0547f3d64ef67f169cbb2261
Begmyrat <bega888@bk.ru>
2019-01-25 18:15:06 -0600

    miltiply method added

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,7 +4,9 @@ public class Calculator {
         double sum=num1+num2;
         return sum;
     }
-
+    public static double multiply(double num1, double num2){
+        return num1*num2;
+    }
 
 
 }
